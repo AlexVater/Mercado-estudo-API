4ef0325fe1efbf7029d5f5ee2dcf385fc69f029c
Alex <alexsander.vater@gmail.com>
2022-08-07 14:33:29 -0300

    add my configuration

--- src/main/java/org/github/mercado/MercadoApplication.java
+++ src/main/java/org/github/mercado/MercadoApplication.java
@@ -1,10 +1,24 @@
 package org.github.mercado;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MercadoApplication {
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+
+    @GetMapping("/hello")
+    public String HelloWorld() {
+        return applicationName;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MercadoApplication.class, args);
---
+++ src/main/java/org/github/mercado/MyConfiguration.java
@@ -0,0 +1,14 @@
+package org.github.mercado;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName() {
+        return "Mercado";
+    }
+
+}
